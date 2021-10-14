732b0f0c68cd39a032a209576c324581fd24afe2
rkeenan <leprechaun424@gmail.com>
2021-10-14 14:57:49 -0600

    Adding parameterized param names and the ability to log a single sample. Updated README with instructions

--- README.md
+++ README.md
@@ -4,5 +4,25 @@ Steps to use:
 1. Run `npm install`
    1. Note: if [ibt-telemetry](https://github.com/SkippyZA/ibt-telemetry) is not available via npm registry, you'll have to clone it and run `npm install` in its root directory first
 2. Copy/paste your `.ibt` file into the main directory and rename it `telemetry_file.ibt`
-3. Run `npm run csv`
-4. It may take a while but eventually a file called `output.csv` should be created
+3. Run `npm run csv` 
+   1. This will generate a CSV with the default params, see Usage below for more details on customizing
+4. A file called `output.csv` should be created in the main directory
+
+## Usage
+
+There are parameters able to be passed to the script to change functionality.
+
+| Param | Description |
+|---|---|
+| params | Should be passed in as a comma-delimited value. Determines which columns are pulled from the IBT file. e.g. Speed,Throttle |
+| logSample | Used to output a single sample as reference for the currently available fields. Pass a boolean value - true/false |
+
+###Example parameterized usages
+
+| Command | Output |
+|---|---|
+| `npm run csv -- --params=Speed,Throttle` | Will generate a CSV with only Speed and Throttle columns |
+| `npm run csv -- --params=Speed,Throttle --logSample=true` | Will generate a CSV the same as above but will also output a single sample |
+| `npm run csv -- --logSample=true` | Will generate a CSV with the default fields (see `index.js`) and also output a single sample |
+
+Note: all the double hyphens above are necessary 
--- index.js
+++ index.js
@@ -4,18 +4,34 @@ const telemetryLib = require('ibt-telemetry')
 const fs = require('fs')
 
 const pathToIbt = './telemetry_file.ibt'
-const logOne = false;
+let logSample = false
+let params = []
+
+var argv = require('minimist')(process.argv.slice(2))
+
+if (argv.params && argv.params !== '') {
+    params = argv.params.split(',')
+}
+if (argv.logSample && argv.logSample !== '') {
+    logSample = argv.logSample
+}
 
 // Change this list manually (for now) if you want to change the params output in the CSV
-const params = ['Speed','Throttle','Brake','Clutch','Gear','RPM','LapDistPct','LapDist','Lap','LapBestLap','LapBestLapTime','TrackTemp','PlayerCarTeamIncidentCount','PlayerCarMyIncidentCount','PlayerCarDriverIncidentCount','Lat','Lon','SteeringWheelAngle','FuelLevel']
+const defaultParams = ['Speed','Throttle','Brake','Clutch','Gear','RPM','LapDistPct','LapDist','Lap','LapBestLap','LapBestLapTime','TrackTemp','PlayerCarTeamIncidentCount','PlayerCarMyIncidentCount','PlayerCarDriverIncidentCount','Lat','Lon','SteeringWheelAngle','FuelLevel']
+if (params.length === 0) {
+    params = defaultParams
+}
+
+console.log('Generating CSV using the following params:')
+console.log(params)
 
 let csvString = params.toString() + "\n";
 const telemetry = telemetryLib.Telemetry.fromFile(pathToIbt).then((promisedData) => {
     let index = 0
     for (sample of promisedData.samples()) {
-        if (logOne && index == 1) {
+        if (logSample && index == 1) {
+            console.log('Logging single sample:')
             console.log(sample.toJSON())
-            break;
         }
         let current = [];
         for (paramIndex in params) {
